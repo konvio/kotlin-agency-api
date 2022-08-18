27d530b8dccde91d0bf2ebaf4e7a4ddff1bcf7e6
Vitaliy Kononenko <v@kononenko.tech>
2022-08-18 21:41:32 +0300

    Cors config

--- src/main/kotlin/tech/kononenko/agency/config/CorsConfig.kt
+++ src/main/kotlin/tech/kononenko/agency/config/CorsConfig.kt
@@ -1,11 +1,9 @@
 package tech.kononenko.agency.config
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-@Profile("dev")
 @Configuration
 class CorsConfig : WebMvcConfigurer {
 
