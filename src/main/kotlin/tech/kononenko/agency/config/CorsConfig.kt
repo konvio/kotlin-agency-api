ca6a3aa4cda18402275df7adecf2fff7b86927af
Vitaliy Kononenko <v@kononenko.tech>
2022-08-19 09:31:32 +0300

    Modify cors config

--- src/main/kotlin/tech/kononenko/agency/config/CorsConfig.kt
+++ src/main/kotlin/tech/kononenko/agency/config/CorsConfig.kt
@@ -8,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class CorsConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**").allowedMethods("*")
+        registry.addMapping("/**")
+            .allowedMethods("*")
+            .allowedHeaders("*")
     }
 }
